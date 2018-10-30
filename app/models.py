b1ad339679ef6a0f75c2c779588990ec0e018f1e
kashamalasha <dmitry.burnyshev@gmail.com>
2018-10-30 20:57:09 +0300

    добавление аватара в профиль, вынесение постов в подшаблон

--- app/models.py
+++ app/models.py
@@ -3,6 +3,7 @@ from app import db
 from app import login
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
+from hashlib import md5
 
 
 class User(UserMixin, db.Model):
