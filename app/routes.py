a8e60e25dbffd4de7c2fe4b8f33dcbd8ce0184d0
kashamalasha <dmitry.burnyshev@gmail.com>
2018-11-13 19:35:48 +0300

    Добавлена страница для восстановления пароля

--- app/routes.py
+++ app/routes.py
@@ -2,9 +2,10 @@ from app import app, db
 from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
-from app.forms import LoginForm, RegistrationForm, EditProfileForm, PostForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm, PostForm, ResetPasswordRequestForm
 from app.models import User, Post
 from datetime import datetime
+from app.email import send_email
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -135,6 +136,20 @@ def explore():
     prev_url = url_for('explore', page=posts.prev_num) if posts.has_prev else None
     return render_template('index.html.j2', title='Explore', posts=posts.items,
                            next_url=next_url, prev_url=prev_url)
+
+
+@app.route('/reset_password_request', methods=['GET', 'POST'])
+def reset_password_request():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = ResetPasswordRequestForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user:
+            send_email(user)
+        flash('Check your email for the instructions to reset your password')
+        return redirect(url_for('login'))
+    return render_template('reset_password_request.html.j2', title='Reset password', form=form)
 
 
 @app.before_request
